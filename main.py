165e0ce81f982c5f03ce42a50414756a35d1ca88
Christian Bøgelund <s171243@student.dtu.dk>
2022-11-24 15:31:12 +0100

    Added main file that preprocesses data and splits the users

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-# This is a sample Python script.
+from clustering import split_users
+from data.data_loader import load_data_raw
+from data.data_preprocessing import preprocess_df, extract_additional_user_features
+from data.feature_categorization import U_features
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    df_u, df_pr, df_c = load_data_raw(subset=False)
+    print("data loaded")
+    X = extract_additional_user_features(df_u, df_pr, df_c)
+    print("Features extracted")
+    user_features = U_features()
+    X = preprocess_df(df=X, o_features=user_features)
+    print("Data preprocessed")
+    dfs, labels = split_users(X)
+    print("Users split")
+    SPLIT_USERS = False
+    if SPLIT_USERS:
+        for df, label in zip(dfs, labels):
+            # INSERT CLUSTERING AND OTHER STUFF HERE
+            pass
+    else:
+        # INSERT CLUSTERING AND OTHER STUFF HERE
+        pass
