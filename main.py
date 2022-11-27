04e0318d080a421b88cf7e84c044e3c691d9f7e1
Madscba <Madscba@gmail.com>
2022-11-27 13:14:34 +0100

    initial steps to integrate recommender system

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def log(msg):
 
 def main():
     log("Loading data...")
-    df_u, df_pr, df_c = load_data_raw(subset=True)
+    df_u, df_pr, df_c = load_data_raw(subset=False)
 
     log("Feature extraction...")
     X = extract_additional_user_features(df_u, df_pr, df_c)
@@ -29,29 +29,51 @@ def main():
     user_features = U_features()
     X = preprocess_df(df=X, o_features=user_features)
 
-    log("Splitting users...")
-    dfs, labels = split_users(X)
-    SPLIT_USERS = False
+
+    SPLIT_USERS = True
     if SPLIT_USERS:
+        log("Splitting users...")
+        dfs, labels = split_users(X)
+        del X
         for df, label in zip(dfs, labels):
             # INSERT CLUSTERING AND OTHER STUFF HERE
+            log("Getting clusters for {} users...".format(label))
+            get_clusters_and_similarity_matrix(df)
             pass
     else:
         # INSERT CLUSTERING AND OTHER STUFF HERE
+        get_clusters_and_similarity_matrix(X)
         pass
 
+
+
+    pass
+    #TODO add segmentation
+    # M, U1_ids, P1_ids = generate_utility_matrix_for_one_cluster(clusters=clusters, df_u_full=df_u, df_pr_full=df_pr, cluster_id=cluster_id)
+    #
+    # #U[i:(cluster.shape[0] + i), :] = M
+    # #i += cluster.shape[0]
+    # user_idx = 1 #U1_ids.iloc[0]
+    # difficulties_for_single_user = get_psedu_problem_difficulties_for_single_user(user_idx, M)
+    #
+    # difficulties_for_all_users = get_psedu_problem_difficulties(M)
+    #
+    # recommendation_difficulty_for_single_user, recommendation_idx_single = get_recommendation(difficulties_for_single_user)
+    # recommendation_difficulty_for_all_users,recommendation_idx_all = get_recommendation(difficulties_for_all_users)
+
+
+def get_clusters_and_similarity_matrix(X):
     log("CURE Classification...")
     data = X.drop(columns=["uuid"]).to_numpy()
     cure_repres = cure_representatives(data)
     labels = cure_classify(data, cure_repres)
     log("CURE Classification: done")
-
     n_clusters = labels.max() + 1
     partition = lambda k: data[labels == k]
     similarities = [pairwise_distances(partition(k)) for k in range(n_clusters)]
     sim_users = [X["uuid"][labels == k] for k in range(n_clusters)]
     log("Similarity matrices calculated")
-
+    return
 
 if __name__ == '__main__':
     main()
