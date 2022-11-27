6ebd19f20b8488e6da279208dd427ba63fabacd5
s175549 <s175549@student.dtu.dk>
2022-11-28 00:26:05 +1100

    Evaluation of recommendations

--- sandbox/recommender_system.py
+++ sandbox/recommender_system.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
+import random
 from sklearn.preprocessing import StandardScaler
 #from clustering import user_clustering_kmeans, visualize_with_PCA
 from data.data_loader import load_data_raw
@@ -27,9 +28,10 @@ def get_clusters():
     clusters = pd.read_csv(r'data/csv_files/clusters.csv')
     return clusters
 
-def construct_util_matrix(users,problems):
+def construct_util_matrix(users,problems,test_index):
     unique_problems = problems['upid'].unique()
     M = np.empty((len(users),len(unique_problems)))
+    M_test = np.zeros_like(M)
     for i, userID in enumerate(tqdm(users['uuid'],desc="Func: construct_util_matrix()")):
         user_problems = problems.loc[problems['uuid'] == userID]
         if not user_problems.empty:
@@ -44,16 +46,20 @@ def construct_util_matrix(users,problems):
                 time = user_problems.loc[[j],'total_sec_taken'].values[0]
                 is_correct = user_problems.loc[[j],'is_correct'].values[0]
                 user_difficulties.append(get_difficulty(time,max_time,is_correct))
-                M[i,problem_index] = user_difficulties[-1]
+                M[i, problem_index] = user_difficulties[-1]
             #standardize difficulty values between [0,1]
             min_ = np.min(user_difficulties)
             max_ = np.max(user_difficulties)
             M[i,user_prob_idx] = (M[i,user_prob_idx] - min_) / (max_)
-
+            # Save 'true' difficulty measures and remove from utility matrix
+            for idx in user_prob_idx:
+                if idx in test_index:
+                    M_test[i, idx] = M[i, idx]
+                    M[i, idx] = 0.0
         else:
             #M = np.delete(M, i, axis=0)
             pass
-    return M,unique_problems
+    return M, M_test, unique_problems
 
 def get_difficulty(time,max_time,is_correct,alpha=0.8):
     """
@@ -68,6 +74,20 @@ def get_difficulty(time,max_time,is_correct,alpha=0.8):
     difficulty = 1-((max_time - time) / max_time)*alpha - beta*is_correct
     return difficulty
 
+def split_data(users,problems,random=False):
+    if random:
+        pr_test = random.choices(problems.index.to_list(),k=int(len(problems.index.to_list())/5))
+    else:
+        pr_sorted = problems.sort_values(by='timestamp_TW')
+        test_index = []
+        for user in tqdm(users['uuid'].to_list()):
+            pr_user = pr_sorted[pr_sorted['uuid']==user]
+            num_pr = len(pr_user)
+            num_train = int(num_pr*0.8)
+            if num_pr > 1:
+                test_index.append(pr_user.index.to_list()[num_train:])
+        pr_test = [item for sublist in test_index for item in sublist]
+    return pr_test
 
 def get_utility_matrix_shape(clusters,df_pr,subset=False):
     if subset:
@@ -84,7 +104,7 @@ def get_utility_matrix_shape(clusters,df_pr,subset=False):
         return clusters.shape[0], len(df_pr['upid'].unique())
 
 
-def generate_utility_matrix_for_one_cluster(clusters,cluster_id,df_u_full,df_pr_full):
+def generate_utility_matrix_for_one_cluster(clusters,cluster_id,df_u_full,df_pr_full,test_index):
     cluster = clusters.loc[clusters['cluster'] == cluster_id]
     sub_users = df_u_full['uuid'].isin(cluster['uuid'].to_list())
     df_u_sub = df_u_full.loc[sub_users]
@@ -97,29 +117,33 @@ def generate_utility_matrix_for_one_cluster(clusters,cluster_id,df_u_full,df_pr_
     # sub_problems = df_pr_full['upid'].isin(problems)
     # df_pr_sub = df_pr_full.loc[sub_problems]
     df_pr_sub = df_pr_full.loc[df_pr_full['uuid'].isin(df_u_sub['uuid'])]
-    M, unique_prob_ids = construct_util_matrix(df_u_sub, df_pr_sub)
-    return M, df_u_sub['uuid'], unique_prob_ids
+    M, M_test, unique_prob_ids = construct_util_matrix(df_u_sub, df_pr_sub,test_index)
+    return M, M_test, df_u_sub['uuid'], unique_prob_ids
 
 
-def get_psedu_problem_difficulties(M):
+def get_psedu_problem_difficulties(M, M_test):
     # Now that we have a utility matrix, we need to fill all empty entries
+    errors = []
     recommendations = np.zeros_like(M)  # Create copy of utility matrix, so that 'predictions' are not used when aggregating
     for user in tqdm(range(np.shape(M)[0]),desc="get_psedu_problem_difficulties()"):
-        recommendations[user, :] = get_psedu_problem_difficulties_for_single_user(user, M)
-    return recommendations
-def get_psedu_problem_difficulties_for_single_user(user_idx, M):
+        recommendations[user, :], error = get_psedu_problem_difficulties_for_single_user(user, M, M_test)
+        errors.append(error)
+    return recommendations, errors
+
+def get_psedu_problem_difficulties_for_single_user(user_idx, M, M_test):
     # Now that we have a utility matrix, we need to fill all empty entries
     #M2 = np.copy(M)  # Create copy of utility matrix, so that 'predictions' are not used when aggregating
     n_user = np.shape(M)[0]
     n_problems = np.shape(M)[1]
     user_recommendations = np.zeros(n_problems)
-    unsolved_problems = np.argwhere(M[user_idx, :] == 0)
+    unsolved_problems = np.argwhere(M_test[user_idx, :] != 0.0)
     unsolved_problems = [u[0] for u in unsolved_problems]
     relevant_user_ids = list(range(0,user_idx))+list(range(user_idx+1,n_user))
     relevant_M = M[relevant_user_ids,:][:,unsolved_problems]
     user_recommendations[unsolved_problems] = np.sum(relevant_M, axis=0) / np.sum(relevant_M != 0, axis=0)
     user_recommendations[np.isnan(user_recommendations)] = 0
-    return user_recommendations
+    errors = np.abs(user_recommendations[unsolved_problems]-M_test[user_idx,unsolved_problems])
+    return user_recommendations, errors
 
 def get_recommendation(difficulty_matrix,quantile=0.80,recommendations_to_return = 1):
     if len(difficulty_matrix.shape) == 1:
@@ -151,20 +175,22 @@ if __name__ == "__main__":
     clusters = get_clusters()
     #rows,columns = get_utility_matrix_shape(clusters,df_pr,subset=sub)
     #U = np.empty((rows,columns))
-
+    test_index = split_data(df_u,df_pr)
 
     #TODO Generate utility matrix for each cluster - and save.
     i = 0
     cluster_id = 3
-    M, U1_ids, P1_ids = generate_utility_matrix_for_one_cluster(clusters=clusters, df_u_full=df_u, df_pr_full=df_pr, cluster_id=cluster_id)
+    M, M_test, U1_ids, P1_ids = generate_utility_matrix_for_one_cluster(clusters=clusters,
+                                                                df_u_full=df_u, df_pr_full=df_pr,
+                                                                cluster_id=cluster_id, test_index=test_index)
 
     #U[i:(cluster.shape[0] + i), :] = M
     #i += cluster.shape[0]
     user_idx = 1 #U1_ids.iloc[0]
-    difficulties_for_single_user = get_psedu_problem_difficulties_for_single_user(user_idx, M)
-
-    difficulties_for_all_users = get_psedu_problem_difficulties(M)
-
+    difficulties_for_single_user, errors_single = get_psedu_problem_difficulties_for_single_user(user_idx, M, M_test)
+    difficulties_for_all_users, errors_all = get_psedu_problem_difficulties(M,M_test)
+    errors = [item for sublist in errors_all for item in sublist]
+    mean_abs_error = np.mean(errors)
     recommendation_difficulty_for_single_user, recommendation_idx_single = get_recommendation(difficulties_for_single_user)
     recommendation_difficulty_for_all_users,recommendation_idx_all = get_recommendation(difficulties_for_all_users)
 
